c49f3fd20f2dd0a129be747e1b7d5abec40197b7
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-10-03 14:56:50 +0530

    Update and rename send_file.pj to send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -18,7 +18,7 @@ async def reply_forward(message: Message, file_id: int):
         # get caption (if any)
         caption = message.caption if media.file_name else ""
         await message.reply_text(
-            f"**Files will be Deleted After 15 min**\n\n"
+            f"**Files will be Deleted After 1 min**\n\n"
             f"**__To Retrive the Stored File, just again open the link!__**\n\n"
             f"**{caption}\n\nLink:** https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(str(file_id))}",
             disable_web_page_preview=True, quote=True)
@@ -43,8 +43,8 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 1800))
+    asyncio.create_task(delete_after_delay(sent_message, 60))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
-    await send_message.delete()
+    await asyncio.send_message.delete()
