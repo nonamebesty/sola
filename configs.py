f91e20e30e59b2fb73b44c83221fb828c2402203
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-11-12 08:39:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,8 +7,8 @@ import os
 class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "5961170420:AAEC8tsWFZp88LpbTMOe7pqS-HqUbh_MvC0"
-	BOT_USERNAME = "asumoviessharebot"
+	BOT_TOKEN = "6986947579:AAEsGcSmos_k65HyEgl33EB7-a5vwjaCfKk"
+	BOT_USERNAME = "asumoviessharesbot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
