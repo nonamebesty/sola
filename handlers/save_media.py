5eff288af160919d95b71e7c6d8385b802b01aac
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-01 12:15:38 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -89,10 +89,10 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             ]])
         )
         share_link = f"https://nammatvserial.jasurun.workers.dev/?start=JAsuran_{str_to_b64(str(SaveMessage.id))}"
-        
+        short_link = await get_short(share_link)
         #share_link = f"📤 Size: 500MB\n\n🎫 Quality: All\n\n🎧 Audio : Tamil\n\nhttps://nammatvserial.jasurun.workers.dev/?start=JAsuran_{str_to_b64(str(SaveMessage.id))}"
         await editable.edit(
-            f"**ᴀʟʟ ɪɴ 1:** {share_link}",
+            f"**ᴀʟʟ ɪɴ 1:** {short_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://telegram.me/As_botzz"),
